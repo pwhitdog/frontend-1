59a64d60df253132534536dc92352c0da7252a58
Gonzalo Scannone <gonzalo.scannone@gosolucionesysistemas.com.ar>
2021-05-10 19:56:09 -0300

    add build

---
+++ build/static/js/19.91a07af8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[19],{1101:function(n,e,a){"use strict";a.r(e);var t=a(1086);e.default=t.Line}}]);
+//# sourceMappingURL=19.91a07af8.chunk.js.map
---
+++ build/static/js/21.b7cc47bd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[21],{946:function(n,p){}}]);
+//# sourceMappingURL=21.b7cc47bd.chunk.js.map
